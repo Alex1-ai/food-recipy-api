53ea1372cc54b58b22839e85a6073caa421e669d
Alex1.ai <alexanderemmanuel1719@gmail.com>
2024-03-24 12:48:15 +0000

    testing admin and adding drf-spectacular

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,4 +1,47 @@
-# the noqa tells flake8 to ignore that line for linting
-from django.contrib import admin # noqa 
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """ Define the admin pages for users"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        # None means the title in the admin field
+        (None, {"fields": ("email", "password")}),
+        (
+           _('Permissions'),
+           {
+               'fields': (
+                   'is_active',
+                   'is_staff',
+                   'is_superuser'
+               )
+           }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            # classes is used to add css styles to the admin page
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)

ad17e8352535ed1b77f0a8bbdfdadb0a09644c64
Alex1.ai <alexanderemmanuel1719@gmail.com>
2024-06-12 22:36:14 +0000

    added health check api

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,10 +21,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # generate schema file needed for the project
+    path('api/health-check/',core_views.health_check, name='health-check'),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
